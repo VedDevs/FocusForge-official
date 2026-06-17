5ce3b189d883cac32cbf2015ff9141b8ecf1ccef
Vedant <veddevs77@gmail.com>
2026-06-17 17:05:18 +0530

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,28 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBNBj7etKpMBGUbmJeFF_bpOGs1b3wp7mI",
+  authDomain: "studyxp-b9503.firebaseapp.com",
+  projectId: "studyxp-b9503",
+  storageBucket: "studyxp-b9503.firebasestorage.app",
+  messagingSenderId: "818596724239",
+  appId: "1:818596724239:web:d799dc0649dddd6d852d7a"
+};
+
+export const USERS = {
+  vedant: {
+    id: "vedant",
+    name: "Nahi Aati",
+    color: "var(--vedant)"
+  },
+  akshaya: {
+    id: "akshaya",
+    name: "abcd",
+    color: "var(--akshaya)"
+  }
+};
+
+export const USER_IDS = Object.keys(USERS);
+
+export const REWARD_ICONS = [
+  "🎮","🍕","🎬","📚","🎵","⚽",
+  "🏆","🍦","🎁","✈️","💻","🎯"
+];
